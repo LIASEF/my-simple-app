22b61a2eabca2ae3fedac2161d8c25b0d7805813
KELVIN <kirmarkel@gmail.com>
2025-04-20 14:07:36 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Мой Стартап</h1>
+      <nav>
+        <a href="#home">Главная</a>
+        <a href="#about">О нас</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
